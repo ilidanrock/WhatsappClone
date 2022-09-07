ccbe91396dc8aef90f77fcbc46bd73b235f51d11
LUIS RIVERA <lrivera@optecllc.com>
2022-09-06 20:47:12 -0500

    amplify init

--- App.tsx
+++ App.tsx
@@ -4,6 +4,10 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+Amplify.configure(awsconfig)
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
